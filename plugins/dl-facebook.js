2dcc1def1bcc92443caca6100f85555028200fb3
MR Katussa <108258060+Mr-Katussa@users.noreply.github.com>
2023-03-09 21:15:23 -0800

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,20 +2,22 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
  
- if (!args[0]) throw `✳️ Envíe el link de un video de Facebook\n\n📌 Ejemplo :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
+ if (!args[0]) throw `PLEASE ENTER THE FB LINK\n\n📌EX :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
     m.react(rwait)
    try {
    let res = await fg.fbdl(args[0])
     for (let result of res.download) {
     	  let tex = `
-┌─⊷ *FBDL*
-▢ *Calidad:* ${result.quality}
+┌─⊷ *FB DOWNLODER*
+▢ *Quality
+
+:* ${result.quality}
 └───────────`
     conn.sendFile(m.chat, result.url, 'fb.mp4', tex, m)
      } 
      m.react(done)
  } catch {
- 	m.reply('Error: Intente de nuevo con otro link')
+ 	m.reply('Error')
  	} 
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
